2c1d1fecaa4215ce44999b2ed0022fd97c72475a
ASuherman <AnggaSuherman@users.noreply.github.com>
2023-01-12 19:18:42 +0900

    Update Store.rs

--- src/griddb/Store.rs
+++ src/griddb/Store.rs
@@ -14,7 +14,7 @@
     limitations under the License.
 */
 
-extern crate griddbnet-sys as ffi;
+extern crate griddbnet_sys as ffi;
 
 use crate::griddb::Container::*;
 use crate::griddb::ContainerInfo::*;
